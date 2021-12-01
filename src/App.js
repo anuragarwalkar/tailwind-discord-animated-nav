23bfbaf739f2374b51fe6a1b7f8d14b06840beac
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-12-01 19:39:09 +0530

    rename

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <p className="text-center text-green-500">Hi...</p>;
+}
+
+export default App;
--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <p className="text-center text-green-500">Anurag Arwalkar</p>
-  );
-}
-
-export default App;
